11df0ff53673cd96a910fc2994e2ba52522d71bc
xthazudp <xthazudp@gmail.com>
2020-08-11 11:27:53 +0545

    starting real project

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-import str from './models/Search';
-
-// import {add as a, multiply as m, ID} from './views/searchView';
-import * as searchView from './views/searchView';
-
-console.log(`using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`); 
--- src/js/views/searchView.js
+++
@@ -1,5 +0,0 @@
-// const { compilation } = require("webpack");
-
-export const add = (a, b) => a + b;
-export const multiply = (a, b) => a * b;
-export const ID = 23;
