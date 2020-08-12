b4885c59762193b662b0bef833dd9eedb937f78e
xthazudp <xthazudp@gmail.com>
2020-08-12 11:04:26 +0545

    search page pagination

--- src/js/index.js
+++ src/js/index.js
@@ -41,3 +41,11 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
+elements.searchRes.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
