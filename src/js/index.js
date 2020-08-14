34530b5a947b872a7b881caab25923aafa3a0b07
xthazudp <xthazudp@gmail.com>
2020-08-14 17:34:15 +0545

    likes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
@@ -129,6 +130,43 @@ const controlList = () =>{
     });
 }
 
+/////////////////////////////
+// LIKES CONTROLLER
+/////////////////////////////
+
+const controlLike = () => {
+    if(!state.likes) state.likes = new Likes();
+    const currentID =state.recipe.id;
+
+    // User has NOT liked current recipe yet
+    if(!state.likes.isLiked(currentID)) {
+        // Add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        );
+
+        // toggle the like button
+
+        // add like to the UI list
+        console.log(state.likes);
+        
+    // User has liked current recipe
+    } else{
+        // Remove like from the state
+        state.likes.deleteLike(currentID);
+
+        // toggle the like button
+
+        // add like to the UI list
+        console.log(state.likes);
+
+    }
+};
+
+
 // Handle delete and update list item events
 elements.shopping.addEventListener('click', e => {
     const id = e.target.closest('.shopping__item').dataset.itemid;
@@ -162,7 +200,12 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     } else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        // Add ingredients to shopping list
         controlList();
+    } else if(e.target.matches('.recipe__love, .recipe__love *')) {
+        // Like controller
+        controlLike();
+
     }
     console.log(state.recipe);
 });
